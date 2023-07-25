8f5eee13d2caacba6f63fc50c141175fc6eda7e6
Muhammed <muhammedturfan@icloud.com>
2023-07-25 12:47:14 -0700

    new solved problem sent. 7/25/23

---
+++ collection_map_tasks/Map_FrequencyOfCharacters_SOLVING.java
@@ -0,0 +1,23 @@
+package collection_map_tasks;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Map_FrequencyOfCharacters_SOLVING {
+
+    public static void main(String[] args) {
+        String str = "muhammed";
+
+        System.out.println(frequentCharacter(str));
+
+    }
+
+    static Map<Character,Integer> frequentCharacter(String str){
+        Map<Character,Integer> map = new LinkedHashMap<>();
+
+        for(char each : str.toCharArray()){
+            map.put(each, map.getOrDefault(each, 0)+1);
+        }
+        return map;
+    }
+}
