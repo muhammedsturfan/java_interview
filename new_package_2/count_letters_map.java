30b861cb35e467e939425b7f220a81235b999425
Muhammed <muhammedturfan@icloud.com>
2023-09-11 20:37:50 -0700

    3 problem solved 9/11/23

--- new_package_2/count_letters_map.java
+++ new_package_2/count_letters_map.java
@@ -1,4 +1,30 @@
 package new_package_2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class count_letters_map {
+
+    /*
+        input
+        String = aabbccc
+        output
+        {a=2, b=2, c=3}
+     */
+
+    public static void main(String[] args) {
+        String str = "aabbccc";
+        for (Map.Entry<Character,Integer> newMap : countLetters(str).entrySet()){
+            System.out.println(newMap);
+        }
+    }
+
+    static HashMap<Character,Integer> countLetters(String str){
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < str.length(); i++){
+            map.put(str.charAt(i), map.getOrDefault(str.charAt(i),0)+1);
+        }
+        return map;
+    }
 }
